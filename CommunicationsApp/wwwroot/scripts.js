58c881065d75fa7d36fa65400bba8b52a7d570f8
paavkar <paavo_karppinen@outlook.com>
2025-06-22 21:10:06 +0300

    improve the scrolling

--- CommunicationsApp/wwwroot/scripts.js
+++ CommunicationsApp/wwwroot/scripts.js
@@ -19,15 +19,33 @@ window.titleHelper = {
 
 window.chatScroll = {
     isAtBottom: true,
+
     checkScrollLock: function (elementId) {
         const el = document.getElementById(elementId);
         if (!el) return;
-
-        this.isAtBottom = el.scrollHeight - el.scrollTop <= el.clientHeight + 20;
+        const threshold = 100;
+        this.isAtBottom = (el.scrollHeight - el.scrollTop - el.clientHeight) <= threshold;
     },
-    scrollToBottom: function (elementId) {
-        const el = document.getElementById(elementId);
-        if (el && this.isAtBottom) {
+    initializeScrollWatcher: function (elementId) {
+        setTimeout(() => {
+            const el = document.getElementById(elementId);
+            if (!el) return;
+            //console.log("Initializing scroll watcher for:", elementId);
+            window.chatScroll.checkScrollLock(elementId);
+            el.addEventListener('scroll', () => {
+                window.chatScroll.checkScrollLock(elementId);
+                //console.log("Scrolling:", el.scrollTop, el.clientHeight, el.scrollHeight, window.chatScroll.isAtBottom);
+            });
+        }, 50);
+    },
+    shouldScroll: function (scrollContainerId) {
+        window.chatScroll.checkScrollLock(scrollContainerId);
+        //console.log("Should scroll:", window.chatScroll.isAtBottom);
+        return window.chatScroll.isAtBottom;
+    },
+    scrollToBottom: function (endElementId) {
+        const el = document.getElementById(endElementId);
+        if (el) {
             el.scrollIntoView({ behavior: 'smooth', block: 'start' });
         }
     }
