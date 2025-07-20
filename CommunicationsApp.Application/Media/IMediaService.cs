7d7b09f81f0dc34654ff9b502af8b019f8336086
paavkar <paavo_karppinen@outlook.com>
2025-07-20 19:40:26 +0300

    improve file saving naming, add logging on exceptions

--- CommunicationsApp.Application/Media/IMediaService.cs
+++ CommunicationsApp.Application/Media/IMediaService.cs
@@ -4,6 +4,6 @@ namespace CommunicationsApp.Application.Interfaces
 {
     public interface IMediaService
     {
-        Task<List<string>> UploadPostImagesAsync(List<IBrowserFile> files);
+        Task<List<string>> UploadPostImagesAsync(List<IBrowserFile> files, string messageId);
     }
 }
--- CommunicationsApp.Infrastructure/Media/MediaService.cs
+++ CommunicationsApp.Infrastructure/Media/MediaService.cs
@@ -2,28 +2,38 @@
 using CommunicationsApp.Application.Interfaces;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace CommunicationsApp.Infrastructure.Services
 {
-    public class MediaService(IConfiguration configuration) : IMediaService
+    public class MediaService(IConfiguration configuration, ILogger<MediaService> logger) : IMediaService
     {
         readonly string ConnectionString = configuration.GetValue<string>("BlobStorage:DefaultConnection")!;
         readonly string MessageContainerName = configuration.GetValue<string>("BlobStorage:MessageContainerName")!;
 
-        public async Task<List<string>> UploadPostImagesAsync(List<IBrowserFile> files)
+        public async Task<List<string>> UploadPostImagesAsync(List<IBrowserFile> files, string messageId)
         {
             var blobServiceClient = new BlobServiceClient(ConnectionString);
             var containerClient = blobServiceClient.GetBlobContainerClient(MessageContainerName);
 
             List<string> fileUrls = [];
 
+            var i = 0;
             foreach (var file in files)
             {
                 var fileName = file.Name;
-                var blobClient = containerClient.GetBlobClient(fileName);
-                await blobClient.UploadAsync(file.OpenReadStream(), true);
 
-                fileUrls.Add(blobClient.Uri.ToString());
+                try
+                {
+                    var blobClient = containerClient.GetBlobClient($"{messageId}/image{i}/{fileName}");
+                    await blobClient.UploadAsync(file.OpenReadStream(), true);
+                    fileUrls.Add(blobClient.Uri.ToString());
+                    i++;
+                }
+                catch (Exception e)
+                {
+                    logger.LogError(e, "Error uploading file to Blob storage in {Method}", nameof(UploadPostImagesAsync));
+                }
             }
 
             return fileUrls;
