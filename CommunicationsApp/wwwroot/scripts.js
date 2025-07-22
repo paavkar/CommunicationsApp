c0abc2a791f662a3bd560fd18e185bad9796e1f5
paavkar <paavo_karppinen@outlook.com>
2025-07-22 16:48:36 +0300

    add KeyCode check for sending message on just enter press, fix AutoGrow for text area to reset on message send

--- CommunicationsApp/wwwroot/scripts.js
+++ CommunicationsApp/wwwroot/scripts.js
@@ -55,3 +55,7 @@ window.autoGrow = function (element) {
     element.style.height = 'auto';
     element.style.height = element.scrollHeight + 'px';
 };
+
+window.resetHeight = function (element) {
+    element.style.height = 'auto';
+};
