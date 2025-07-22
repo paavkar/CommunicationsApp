f923897852325ceab5ae7c2038d7a1f56ba02bba
paavkar <paavo_karppinen@outlook.com>
2025-07-22 17:14:59 +0300

    add file size to attachments

--- CommunicationsApp.Core/Servers/MediaAttachment.cs
+++ CommunicationsApp.Core/Servers/MediaAttachment.cs
@@ -5,5 +5,6 @@
         public string Url { get; set; }
         public string FileName { get; set; }
         public string? AltText { get; set; }
+        public long FileSize { get; set; }
     }
 }
